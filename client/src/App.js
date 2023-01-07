80ddc075da56568c093c581b39a28eab9f1bcf37
Tyler <tdiloreto929@gmail.com>
2023-01-07 00:22:00 -0500

    Changed check button to delete if owner

--- client/src/App.js
+++ client/src/App.js
@@ -3,119 +3,114 @@ import { useState } from "react";
 import logo from "./gifthub_logo.jpeg";
 
 function Header() {
-    return (
-        <div className="Header">
-            <img src={logo} alt="Really cool logo" className="logo" />
-            <SubmitButton></SubmitButton>
-        </div>
-    );
+  return (
+    <div className="Header">
+      <img src={logo} alt="Really cool logo" className="logo" />
+      <SubmitButton></SubmitButton>
+    </div>
+  );
 }
 
 function SubmitButton() {
-    const [modalOpened, setModalOpened] = useState(false);
-    return (
-        <div className="submitBtn">
-            <p onClick={() => setModalOpened(true)}>+</p>
-            {modalOpened ? <SubmitModal></SubmitModal> : null}
-        </div>
-    );
+  const [modalOpened, setModalOpened] = useState(false);
+  return (
+    <div className="submitBtn">
+      <p onClick={() => setModalOpened(true)}>+</p>
+      {modalOpened ? <SubmitModal></SubmitModal> : null}
+    </div>
+  );
 }
 
 function SubmitModal() {
-    return <div className="submitModal modal">This is the submit modal</div>;
+  return <div className="submitModal modal">This is the submit modal</div>;
 }
 
 const data = [
-    {
-        name: "First",
-        link: "notfound.com",
-        checked: false,
-    },
-    {
-        name: "Second",
-        link: "notfound.com",
-        checked: false,
-    },
-    {
-        name: "Third",
-        link: "notfound.com",
-        checked: true,
-    },
-    {
-        name: "Fourth",
-        link: "notfound.com",
-        checked: true,
-    },
-    {
-        name: "Fifth",
-        link: "notfound.com",
-        checked: false,
-    },
+  {
+    name: "First",
+    link: "notfound.com",
+    checked: false,
+  },
+  {
+    name: "Second",
+    link: "notfound.com",
+    checked: false,
+  },
+  {
+    name: "Third",
+    link: "notfound.com",
+    checked: true,
+  },
+  {
+    name: "Fourth",
+    link: "notfound.com",
+    checked: true,
+  },
+  {
+    name: "Fifth",
+    link: "notfound.com",
+    checked: false,
+  },
 ];
 
-function ListItem({ name, link, isPurchased }) {
-    return (
-        <div className="card w-75 mb-3 ListItem">
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <div className="card-text">{link}</div>
-            </div>
-            <button
-                type="button"
-                className="btn btn-outline-success checkbox"
-                data-bs-toggle="button"
-            >
-                Check
-            </button>
-        </div>
-    );
+function ListItem({ name, link, isPurchased, loginInfo }) {
+  return (
+    <div className="card w-75 mb-3 ListItem">
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <div className="card-text">{link}</div>
+      </div>
+      {loginInfo.listOwner ? <button type="button" className="btn btn-outline-danger checkbox">Delete</button> : <button type="button" className="btn btn-outline-success checkbox" data-bs-toggle="button">Check</button>}
+    </div>
+  );
 }
 
-function List() {
-    const content = data.map((item, index) => (
-        <ListItem
-            name={item.name}
-            link={item.link}
-            isPurchased={item.checked}
-            key={index}
-        ></ListItem>
-    ));
-    return <div className="List">{content}</div>;
+function List({ loginInfo }) {
+  const content = data.map((item, index) => (
+    <ListItem
+      name={item.name}
+      link={item.link}
+      isPurchased={item.checked}
+      key={index}
+      loginInfo={loginInfo}
+    ></ListItem>
+  ));
+  return <div className="List">{content}</div>;
 }
 
 function Spoiler() {
-    const [hideState, setHideState] = useState(false);
+  const [hideState, setHideState] = useState(false);
 
-    if (hideState) {
-        return <div></div>;
-    } else {
-        return (
-            <div className="modal">
-                <h1>Spoiler:</h1>
-                <p>
-                    This is a warning - the following page displays which items
-                    have been purchased.
-                </p>
-                <p>
-                    If you do not wish to be spoiled, turn back! Otherwise,
-                    continue.
-                </p>
-                <button className="continue" onClick={() => setHideState(true)}>
-                    Continue
-                </button>
-            </div>
-        );
-    }
+  if (hideState) {
+    return <div></div>;
+  } else {
+    return (
+      <div className="modal">
+        <h1>Spoiler:</h1>
+        <p>
+          This is a warning - the following page displays which items
+          have been purchased.
+        </p>
+        <p>
+          If you do not wish to be spoiled, turn back! Otherwise,
+          continue.
+        </p>
+        <button className="continue" onClick={() => setHideState(true)}>
+          Continue
+        </button>
+      </div>
+    );
+  }
 }
 
 function App({ login }) {
-    return (
-        <div className="App">
-            {login ? "" : <Spoiler></Spoiler>}
-            <Header></Header>
-            <List></List>
-        </div>
-    );
+  return (
+    <div className="App">
+      {login ? "" : <Spoiler></Spoiler>}
+      <Header></Header>
+      <List loginInfo={login}></List>
+    </div>
+  );
 }
 
 export default App;
