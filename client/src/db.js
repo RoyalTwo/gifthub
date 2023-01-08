bac6df7a57097bf05d5367a339640aac01544a91
Tyler <tdiloreto929@gmail.com>
2023-01-08 15:26:07 -0500

    minrone busg fizde

--- client/src/db.js
+++ client/src/db.js
@@ -24,22 +24,22 @@ export const lists = [
         list: [
             {
                 name: "First",
-                link: "https://discord.com/channels/1045024966321651762/1045037247797399562",
+                link: "https://www.amazon.com/Roblox-Digital-Gift-Card-Exclusive/dp/B0BCCYLB13/ref=sr_1_2?crid=NN3G8SC5D1YO&keywords=robux+gift+card+20&qid=1673209263&sprefix=robux%2520%2Caps%2C91&sr=8-2",
                 checked: false,
             },
             {
                 name: "Second",
-                link: "notfound.com",
+                link: "https://www.bestbuy.com/site/lenovo-ideapad-3i-15-6-hd-touch-laptop-core-i3-1115g4-8gb-memory-256gb-ssd-platinum-grey/6511950.p?skuId=6511950",
                 checked: false,
             },
             {
                 name: "Third",
-                link: "notfound.com",
+                link: "https://discord.com/channels/1045024966321651762/1045037247797399562",
                 checked: true,
             },
             {
                 name: "Fourth",
-                link: "notfound.com",
+                link: "https://www.google.com/search?q=capybara+images&rlz=1C1VDKB_enUS1012US1012&sxsrf=ALiCzsbIbiZztKBlJj6JiynOT40N4DEnkA:1673209186276&source=lnms&tbm=isch&sa=X&sqi=2&ved=2ahUKEwiz4Lr55bj8AhU4MVkFHbHDCj0Q_AUoAXoECAMQAw&biw=1280&bih=609&dpr=1.5",
                 checked: true,
             },
             {
@@ -57,7 +57,7 @@ export const lists = [
         list: [
             {
                 name: "Sixth",
-                link: "notfound.com",
+                link: "https://www.chess.com",
                 checked: false,
             },
             {
